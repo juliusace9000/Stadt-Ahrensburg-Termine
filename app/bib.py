aa1f1c52388b0edfb4100d62877e0baa36d514cd
juliusace9000 <79270157+juliusace9000@users.noreply.github.com>
2022-02-10 11:13:46 +0100

    Changes for armv7

--- app/bib.py
+++ app/bib.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def getTermine():
     options = webdriver.ChromeOptions()
-    driver = webdriver.Remote("http://selenium:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME)
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Remote("http://selenium:4444/wd/hub", desired_capabilities=DesiredCapabilities.CHROME, options=options)
     driver.implicitly_wait(10)
     driver.get('https://reservation.frontdesksuite.com/ahrensburg/ahrensburg/Home/Index?pageId=10963985-24b5-4b55-9b81-a2f9c895905e&culture=de&uiCulture=de')
     driver.find_element_by_xpath("/html/body/div/main/div[2]/div[2]/div/div[2]/a").click()
